d97f7ec32574cdfeb6aa15445bea345e2fb1ba2a
AntsThatWander <anubspider@gmail.com>
2024-04-01 21:00:10 +0900

    Add menu bar on Header.svelte

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -1,47 +1,94 @@
 <script>
 	import { page } from "$app/stores";
-	import logo from "$lib/images/svelte-logo.svg";
 	import github from "$lib/images/github.svg";
+
+	let isMouseOver = false;
+
+	function handleMouseOver() {
+		isMouseOver = true;
+	}
+
+	function handleMouseOut() {
+		isMouseOver = false;
+	}
 </script>
 
 <header>
-	<div class="corner">
+	<div class="left_corner">
 		<a href="/">RNNLA DASHBOARD</a>
 	</div>
-
-	<div class="corner">
+	<div class="menu">
+		<a href="/" aria-current={$page.url.pathname === '/' ? 'page' : undefined}>분석</a>
+		<div class="menu_divider"></div>
+		<a href="/verification" aria-current={$page.url.pathname === '/verification' ? 'page' : undefined}>검증</a>
+	</div>
+	<div class="right_corner">
 		<a href="https://github.com/RNNLA">
 			<img src={github} alt="GitHub" />
 		</a>
 	</div>
 </header>
 
+
+
 <style>
 	header {
 		display: flex;
-		justify-content: space-between;
 		background-color: #33363f;
+		align-items: center;
 	}
 
-	.corner {
-		padding: 1em;
-	}
-
-	.corner a {
+	header a {
 		font-size: large;
 		font-weight: 700;
 		height: 100%;
-		display: flex;
 		align-items: center;
 		color: white;
-	}
+		text-decoration: none;
 
-	.corner img {
+	} 
+
+	header img {
 		width: 2em;
 		height: 2em;
 	}
 
-	a:hover {
-		color: var(--color-theme-1);
+	.left_corner{
+		text-align: center;
+		padding: 1em;
 	}
+
+	.right_corner{
+		margin-left: auto;
+		margin-right: 0.5em;
+	}
+
+	.right_corner a{
+		display: flex;
+		justify-content: center;;
+	}
+
+	.menu{
+		margin-left: 2em;
+		height: 1.2em;
+		display: flex;
+		flex-direction: row;
+		justify-content: center;
+		align-items: center;
+	}
+
+	.menu a{
+		margin-left: 0.5em;
+		margin-right: 0.5em;
+		display: flex;
+		text-align: center;
+	}
+
+	.menu_divider{
+		border-left: 2px solid #ffffff;
+		height: 130%;
+		
+	}
+
+
 </style>
