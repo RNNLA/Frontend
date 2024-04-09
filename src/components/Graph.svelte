2b657bfbcab66ffd99ef49e9e47f1a54bcfe0261
AntsThatWander <anubspider@gmail.com>
2024-04-09 21:21:08 +0900

    Complete Graph

--- src/components/Graph.svelte
+++ src/components/Graph.svelte
@@ -1,6 +1,6 @@
 
 <script lang="ts">
-    import { onMount } from 'svelte';
+    import { onMount, afterUpdate } from 'svelte';
     import * as d3 from 'd3';
   
     // 예시 데이터
@@ -32,132 +32,209 @@
       // {date: '24-03-26',positive: 80, negative: 20},
     ];
 
-    export let svg_width:Number = 400;
-    export let svg_height:Number = 300;
+    let svg_width:number = 400;
+    let svg_height:number = 300;
+    
+    const margin = { top: 10, right: 30, bottom: 70, left: 40 };
+
+    function createChart(){
+        const colors = ["#98C039", "#D43C36"];
+        const width:number = svg_width - margin.left - margin.right;
+        const height:number = svg_height - margin.top - margin.bottom;
+
+        const svg = d3.select('#chart')
+                    .append('svg')
+                    .attr('width', svg_width)
+                    .attr('hieght', svg_height)
+                    .attr('viewBox', `0 0 ${svg_width} ${svg_height}`)
+
+        const chart = svg.append('g')
+                    .attr('transform', `translate(${margin.left},${margin.top})`)
+                    .classed("chart_content", true);
+
+        const x = d3.scaleBand().range([0, width]).domain(data.map(d => d.date)).padding(0.2);
+        const y = d3.scaleLinear().range([height, 0]).domain([0, 100]);
+
+        const keys = ['positive', 'negative'];
+        const stacked_data = d3.stack().keys(keys)(data);
+
+        // x axis
+        chart.append('g')
+            .attr('transform', `translate(0,${height})`)
+            .call(d3.axisBottom(x))
+            .classed('x_axis', true);
+
+        chart.select('.x_axis')
+            .attr("text-anchor", "end")
+            .selectAll("text")
+            .attr("font-weight", "500")
+            .attr("font-size", "1.25rem")
+            .attr("color", "var(--highlight-color)")
+            .attr("transform", "rotate(-45)")
+            .attr("dy", "1.5em")
+            .attr("dx", "1em")
+        
+        // y axis
+        chart.append('g')
+            .call(d3.axisLeft(y))
+            .classed('y_axis', true);
+
+        chart.select('.y_axis')
+              .selectAll('text')
+              .attr("font-size", "1.25rem")
+              .attr("color", "var(--highlight-color)")
+
+        
+        // Drawing Chart
+        const layers = chart.selectAll('.layer')
+                        .data(stacked_data)
+                        .enter()
+                        .append('g')
+                        .classed('layer', true)
+                        .style('fill', (d,i) => colors[i])
+                          .selectAll('rect')
+                          .data(d => d)
+                          .enter()
+                          .append('rect')
+                          .attr('y', d => y(d[1]))
+                          .attr('x', d => x(d.data.date))
+                          .attr('width', x.bandwidth())
+                          .attr('height', d => y(d[0]) - y(d[1]))
+                          .classed("bar", true)
+                          .on("mouseover", onMouseOver)
+                          .on("mouseout", onMouseOut);
+
+        function onMouseOver(event, d){
+          const self = event.target;
+          let color = "";
+          let popup_box_manual_x = 60;
+          let popup_box_manual_x_for_last = popup_box_manual_x * 2.25;
+          d3.select(self)
+            .style("fill", ()=>{
+              color = (d[0] == 0 ? "#4F641A" : "#691A16");
+              return color;
+            })
+
+          const text = chart.append('g')
+                            .attr("id", "popup_box")
+                            
+          const rects_one = document.querySelector("#chart > svg > g > g:nth-child(3)")?.children;
+          const rects_two = document.querySelector("#chart > svg > g > g:nth-child(4)")?.children;
+        
+          
+          text.append("rect")
+              .attr("x",() => x(d3.select(self).datum().data.date) - x.bandwidth()/2+popup_box_manual_x)
+              .attr("y",() => y(d3.select(self).datum()[1]) + (y(d3.select(self).datum()[0]) - y(d3.select(self).datum()[1]))/3)
+              .attr("rx", 20)
+              .attr("ry", 20)
+              .attr("width", "10em")
+              .attr("height", "4em")
+              .classed("popup_rect",true)
+              .style("z-index", "-1")
+              .style("stroke-width", "3px")
+              .style("stroke",color)
+              .style('fill', "white")
+
+            
+            text.append("text")
+                .attr("x",() => x(d3.select(self).datum().data.date) - x.bandwidth()/2+20+popup_box_manual_x)
+                .attr("y",() => y(d3.select(self).datum()[1]) + (y(d3.select(self).datum()[0]) - y(d3.select(self).datum()[1]))/3+25)
+                .attr("id", "popup_box")
+                .text("긍정 기사 : ")
+                .attr("font-weight", "500")
+                .style("fill", "#98C039")
+                .classed("popup_positive_text", true)
+                .append("tspan")
+                .text(`${d3.select(self).datum().data.positive_data}`)
+                .attr("font-weight", "700")
+                .attr("color", "#98C039")
+                .append("tspan")
+                .text("건")
+                .attr("font-weight", "500")
+                .attr("color", "#98C039")
+                
+                text.append("text")
+                .attr("x",() => x(d3.select(self).datum().data.date) - x.bandwidth()/2+20+popup_box_manual_x)
+                .attr("y",() => y(d3.select(self).datum()[1]) + (y(d3.select(self).datum()[0]) - y(d3.select(self).datum()[1]))/3+50)
+                .attr("id", "popup_box")
+                .classed("popup_negative_text", true)
+                .text("부정 기사 : ")
+                .attr("font-weight", "500")
+                .style("fill", "#D43C36")
+                .append("tspan")
+                .text(`${d3.select(self).datum().data.negative_data}`)
+                .attr("font-weight", "700")
+                .append("tspan")
+                .text("건")
+                .attr("font-weight", "500")
+
+                for (let index = 0; index < rects_one.length; index++) {
+                  const element = rects_one[index];
+                  if ((rects_one.length - (index)) <= 3 && element === self) {
+                    console.log(self.getAttribute("x"));
+                    d3.selectAll('.popup_rect')
+                      .attr("x", `${self.getAttribute("x") - popup_box_manual_x_for_last - 20}`);
+                    d3.selectAll('.popup_positive_text')
+                      .attr("x", `${self.getAttribute("x") - popup_box_manual_x_for_last}`);
+                    d3.selectAll('.popup_negative_text')
+                      .attr("x", `${self.getAttribute("x") - popup_box_manual_x_for_last}`);
+                  } 
+                  
+                }
+
+                for (let index = 0; index < rects_two.length; index++) {
+                  const element = rects_two[index];
+                  if ((rects_two.length - (index)) <= 3 && element === self) {
+                    console.log(self.getAttribute("x"));
+                    d3.selectAll('.popup_rect')
+                      .attr("x", `${self.getAttribute("x") - popup_box_manual_x_for_last - 20}`);
+                    d3.selectAll('.popup_positive_text')
+                      .attr("x", `${self.getAttribute("x") - popup_box_manual_x_for_last}`);
+                    d3.selectAll('.popup_negative_text')
+                      .attr("x", `${self.getAttribute("x") - popup_box_manual_x_for_last}`);
+                  } 
+                  
+                }
+
+        }
+
+        function onMouseOut(event, d){
+          const self = event.target;
+          d3.select(self)
+            .style("fill", ()=>{
+              return d[1] != 100 ? "#98C039" : "#D43C36";
+              
+            });
+          
+            d3.selectAll('#popup_box')
+              .remove();
+        }
+
+      }
+
+
+    afterUpdate(()=>{
+        // set the dimensions and margins of the graph
+        
+      const chartDiv = document.querySelector('#chart');
+      svg_width = chartDiv?.parentElement?.clientWidth??svg_width - margin.left - margin.right;
+      svg_height = chartDiv?.parentElement?.clientHeight??svg_height - margin.top - margin.bottom;
+      
+      chartDiv?.firstElementChild?.setAttribute("width", svg_width.toString());
+		  chartDiv?.firstElementChild?.setAttribute("height", svg_height.toString());
+
+      d3.select('svg')
+        .remove()
+
+      createChart();
+      console.log(chartDiv?.firstElementChild);
+  });
 
     
 
 
     onMount(() => {
-      const colors = ["#98C039", "#D43C36"];
-      const highlight_colors = ["", ""]
-      const margin = { top: 20, right: 20, bottom: 100, left: 40 };
-      const width:Number = svg_width - margin.left - margin.right;
-      const height:Number = svg_height - margin.top - margin.bottom;
-
-      const svg = d3.select('#chart')
-                  .append('svg')
-                  .attr('width', svg_width)
-                  .attr('hieght', svg_height)
-                  .attr('viewBox', `0 0 ${svg_width} ${svg_height}`)
-
-      const chart = svg.append('g')
-                  .attr('transform', `translate(${margin.left},${margin.top})`)
-                  .classed("chart_content", true);
-
-      const x = d3.scaleBand().range([0, width]).domain(data.map(d => d.date)).padding(0.2);
-      const y = d3.scaleLinear().range([height, 0]).domain([0, 100]);
-
-      const keys = ['positive', 'negative'];
-      const stacked_data = d3.stack().keys(keys)(data);
-
-      // x axis
-      chart.append('g')
-          .attr('transform', `translate(0,${height})`)
-          .call(d3.axisBottom(x))
-          .classed('x_axis', true);
-
-      chart.select('.x_axis')
-          .attr("text-anchor", "end")
-          .selectAll("text")
-          .attr("transform", "rotate(-90)")
-          .attr("dy", "-0.5em")
-          .attr("dx", "-1em")
-      
-      // y axis
-      chart.append('g')
-          .call(d3.axisLeft(y))
-          .classed('y_axis', true);
-
-
-      
-      
-      // Drawing Chart
-      const layers = chart.selectAll('.layer')
-                      .data(stacked_data)
-                      .enter()
-                      .append('g')
-                      .classed('layer', true)
-                      .style('fill', (d,i) => colors[i])
-                        .selectAll('rect')
-                        .data(d => d)
-                        .enter()
-                        .append('rect')
-                        .attr('y', d => y(d[1]))
-                        .attr('x', d => x(d.data.date))
-                        .attr('width', x.bandwidth())
-                        .attr('height', d => y(d[0]) - y(d[1]))
-                        .classed("bar", true)
-                        .on("mouseover", onMouseOver)
-                        .on("mouseout", onMouseOut);
-
-      function onMouseOver(event, d){
-        const self = event.target;
-        let color = "";
-        d3.select(self)
-          .style("fill", ()=>{
-            color = (d[0] == 0 ? "#4F641A" : "#691A16");
-            return color;
-          })
-
-        const text = chart.append('g')
-        
-        
-        text.append("rect")
-            .attr("x",() => x(d3.select(self).datum().data.date) - x.bandwidth()/2)
-            .attr("y",() => y(d3.select(self).datum()[1]) + (y(d3.select(self).datum()[0]) - y(d3.select(self).datum()[1]))/3)
-            .attr("rx", 20)
-            .attr("ry", 20)
-            .attr("id", "popup_box")
-            .attr("width", x.bandwidth()+40)
-            .attr("height", "4em")
-            .style("stroke-width", "3px")
-            .style("stroke",color)
-            .style('fill', "white")
-          
-          text.append("text")
-              .text(`긍정 기사 : ${d3.select(self).datum().data.positive_data}건`)
-              .attr("x",() => x(d3.select(self).datum().data.date) - x.bandwidth()/2+5)
-              .attr("y",() => y(d3.select(self).datum()[1]) + (y(d3.select(self).datum()[0]) - y(d3.select(self).datum()[1]))/3+25)
-              .attr("id", "popup_box");
-          
-          text.append("text")
-              .text(`부정 기사 : ${d3.select(self).datum().data.negative_data}건`)
-              .attr("x",() => x(d3.select(self).datum().data.date) - x.bandwidth()/2+5)
-              .attr("y",() => y(d3.select(self).datum()[1]) + (y(d3.select(self).datum()[0]) - y(d3.select(self).datum()[1]))/3+50)
-              .attr("id", "popup_box");
-            
-        // chart.append("text")
-        //     .attr("x",() => x(d3.select(self).datum().data.date)-10)
-        //     .attr("y",() => y(d3.select(self).datum()[1])+20) 
-        //     .text(`부정 기사 : ${d3.select(self).datum().data.negative_data}건`);
-
-        //     .attr("stroke","red")
-        //     .attr("stroke-width",2)
-        //     .text(d.x+","+d.y+","+d.r);
-      }
-
-      function onMouseOut(event, d){
-        const self = event.target;
-        d3.select(self)
-          .style("fill", ()=>{
-            return d[1] != 100 ? "#98C039" : "#D43C36";
-            
-          });
-        
-          d3.selectAll('#popup_box')
-            .remove();
-      }
+      createChart();
     });
 </script>
   
@@ -166,13 +243,6 @@
   </div>
 
   <style>
-    #popup_box{
-    }
-
-    .popup_text{
-
-    }
-
   </style>
 
 
--- src/components/WordPreview.svelte
+++ src/components/WordPreview.svelte
@@ -1,9 +1,73 @@
-<script>
-    import wordsImg from "$lib/images/wordsimg.png";
+<script lang='ts'>
+import * as d3 from 'd3';
+import cloud from 'd3-cloud';
+import { onMount, afterUpdate} from 'svelte';
+
+let width:number = 900;
+let height:number = 450;
+let margin = {top: 10, right: 10, bottom: 10, left: 10};
+
+const myWords = [
+    {word: "반도체", size: 100, is_not_risk: true},
+    {word: "삼성", size:200, is_not_risk: true},
+    {word: "금", size:50, is_not_risk: false},
+    {word: "SK머터리얼즈", size:50, is_not_risk: true}
+];
+
+afterUpdate(async()=>{
+        // set the dimensions and margins of the graph
+        
+		const cloudDiv = document.querySelector('#cloud');
+		width = cloudDiv?.parentElement?.offsetWidth??width - margin.left - margin.right;
+		height = cloudDiv?.parentElement?.offsetHeight??height - margin.top - margin.bottom;
+
+		cloudDiv?.firstElementChild?.setAttribute("width", (width).toString());
+		cloudDiv?.firstElementChild?.setAttribute("height", height.toString());
+});
+
+onMount(()=>{
+    // append the svg object to the body of the page
+    var svg = d3.select("#cloud").append("svg")
+                    .attr("width", width + margin.left + margin.right)
+                    .attr("height", height + margin.top + margin.bottom)
+                .append("g")
+                    .attr("transform",
+                        "translate(" + margin.left + "," + margin.top + ")");
+
+    // Constructs a new cloud layout instance. It run an algorithm to find the position of words that suits your requirements
+    // Wordcloud features that are different from one word to the other must be here
+    var layout = cloud()
+                .size([width, height])
+                .words(myWords.map(function(d) { return {text: d.word, size:d.size, is_not_risk:d.is_not_risk}; }))
+                .padding(5)        //space between words
+                .rotate(function() { return ~~(Math.random() * 2) * 90; })
+                .fontSize(function(d) { return d.size; })      // font size of words
+                .on("end", draw);
+    layout.start();
+
+    // This function takes the output of 'layout' above and draw the words
+    // Wordcloud features that are THE SAME from one word to the other can be here
+    function draw(words) {
+        svg.append("g")
+            .attr("transform", "translate(" + layout.size()[0] / 2 + "," + layout.size()[1] / 2 + ")")
+            .selectAll("text")
+                .data(words)
+            .enter().append("text")
+                .style("font-size", function(d) { return d.size; })
+                .style("fill", d => {return d.is_not_risk ? "var(--positive-color)" : "var(--negative-color)"})
+                .attr("text-anchor", "middle")
+                .style("font-family", "Impact")
+                .attr("transform", function(d) {
+                return "translate(" + [d.x, d.y] + ")rotate(" + d.rotate + ")";
+                })
+                .text(function(d) { return d.text; });
+            }
+        });
+
+
+    
 </script>
-<div>
-    <img src={wordsImg} alt="words"/>
-</div>
+<div id="cloud"></div>
 
 
 
@@ -12,7 +76,6 @@
         justify-content: center;
         display: flex;
     }
-    img {
-        width: 65em;
-    }
 </style>
+
+<svelte:window on:load="{()=>handleonload()}"/>
