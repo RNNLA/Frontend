bf997ec6a5e4dd4c3838dc19877608bfbec7cf95
AntsThatWander <anubspider@gmail.com>
2024-04-10 16:21:56 +0900

    Add models for graphs

--- src/store.ts
+++ src/store.ts
@@ -9,4 +9,19 @@ type VerificationNewsDataModel = {
     is_semicon_by_human: boolean;
     is_not_risk_by_human: boolean;
 }
+
+type WordDataModel = {
+    date: string;
+    is_not_risk: boolean;
+}
+
+type WordCloudDataModel = {
+    date: string;
+    word: string;
+    is_not_risk: boolean;
+}
+
+
 export let verificationNewsDataModels = writable<VerificationNewsDataModel[]>([]);
+export let wordDataModels = writable<WordDataModel[]>([]);
+export let wordCloudDataModels = writable<WordCloudDataModel[]>([]);
